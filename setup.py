f263c4bb1c4c3106cc60d2387bdbd2176719cb29
Tazi Mohannad <mohannad.tazi-etu@etu.univh2c.ma>
2024-12-31 23:10:10 +0100

    adding readme

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='tazi_ds',  # Name of your library
     version='1.0.0',
@@ -16,4 +20,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',  # Minimum Python version
+
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
