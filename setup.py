85d46e18b9b8cfa798125c768a3e33ff0f1fc10a
Tazi Mohannad <mohannad.tazi-etu@etu.univh2c.ma>
2024-12-31 23:29:07 +0100

    v1.0.1

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='tazi_ds',  # Name of your library
-    version='1.0.0',
+    name='tazids',  # Name of your library
+    version='1.0.1',
     description='A simple linear regression library',
     author='TAZI Mohannad',
     author_email='mohannadtazi.dev@gmail.com',
